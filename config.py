8c3797e1026aa56b2a1a42b949c2442223e28be6
chenxiaodong <1346650911@qq.com>
2020-07-19 14:52:58 +0800

    add chromedriver

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 
-web_driver_path = "/Users/chenxiaodong/Downloads/chromedriver"
+web_driver_path = "src/chromedriver"
 target_Url = "https://www.maxluck.top/dist/"
+
+
+def set_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('windows-size=1920x1080')
+    return options
 
 
 class drivers:
@@ -11,5 +18,5 @@ class drivers:
 
     def set_driver(self):
         if self.target == "chrome":
-            driver = webdriver.Chrome(executable_path=self.path)
+            driver = webdriver.Chrome(executable_path=self.path, chrome_options=set_options())
             return driver
